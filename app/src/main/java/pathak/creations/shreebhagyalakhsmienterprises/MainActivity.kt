cf1bbde6136226ae4b2b781c9ceffbda0c1ee05c
Anikesh Pathak <anikeshpathak11@gmail.com>
2020-12-01 00:45:56 +0530

    initial commit

---
+++ app/src/main/java/pathak/creations/shreebhagyalakhsmienterprises/MainActivity.kt
@@ -0,0 +1,32 @@
+package pathak.creations.shreebhagyalakhsmienterprises
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import pathak.creations.shreebhagyalakhsmienterprises.welcome.WelcomeActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+
+
+                startActivity(Intent(this, WelcomeActivity::class.java))
+                finish()
+
+
+        },3000)
+
+    }
+
+
+}
---
+++ app/src/main/java/pathak/creations/shreebhagyalakhsmienterprises/welcome/WelcomeActivity.kt
@@ -0,0 +1,17 @@
+package pathak.creations.shreebhagyalakhsmienterprises.welcome
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import pathak.creations.shreebhagyalakhsmienterprises.R
+
+class WelcomeActivity:AppCompatActivity(){
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.welcome)
+
+    }
+
+}
